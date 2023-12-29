e41288b465d6a9894288611c27db43d89e512769
Michiel Stragier <michiel.stragier@student.howest.be>
2023-12-29 01:19:47 +0100

    create custom recent-transaction controller

--- src/api/recent-transaction/controllers/recent-transaction.js
+++ src/api/recent-transaction/controllers/recent-transaction.js
@@ -6,4 +6,21 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recent-transaction.recent-transaction');
+module.exports = createCoreController('api::recent-transaction.recent-transaction', ({strapi})=> ({
+  ...createCoreController('api::recent-transaction.recent-transaction'),
+
+  async findCurrentUserRecentTransactions(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized(`You're not authorized!`);
+    }
+
+    const recentTransactions = await strapi.entityService.findMany('api::recent-transaction.recent-transaction',{
+      filters: { user: user.id },
+      populate: ['duration','parking','payment']
+    })
+
+    return recentTransactions;
+  }
+}));
