dd2181216258c7c60f84f4801c734fa8b75b007d
Michiel Stragier <michiel.stragier@student.howest.be>
2023-12-29 01:40:25 +0100

    filtering the json output

--- src/api/recent-transaction/controllers/recent-transaction.js
+++ src/api/recent-transaction/controllers/recent-transaction.js
@@ -18,9 +18,52 @@ module.exports = createCoreController('api::recent-transaction.recent-transactio
 
     const recentTransactions = await strapi.entityService.findMany('api::recent-transaction.recent-transaction',{
       filters: { user: user.id },
-      populate: ['duration','parking','payment']
+      populate: {
+        duration: true,
+        parking: true,
+        payment: {
+          populate: {
+            currency: true
+          }
+        }
+      }
     })
 
-    return recentTransactions;
+    const filteredRecentTransactions = recentTransactions.map(transaction => {
+      const { createdAt, publishedAt, updatedAt, duration, parking, payment, ...restTransaction } = transaction;
+
+      const filteredDuration = duration ? {
+        ...duration,
+        createdAt: undefined,
+        publishedAt: undefined,
+        updatedAt: undefined
+      } : null;
+
+      const filteredParking = parking? {
+        ...parking,
+        createdAt: undefined,
+        publishedAt: undefined,
+        updatedAt: undefined
+      } : null;
+
+      const filteredPayment = payment ? {
+        ...payment,
+        createdAt: undefined,
+        publishedAt: undefined,
+        updatedAt: undefined,
+        currency: payment.currency ? {
+          ...payment.currency,
+          createdAt: undefined,
+          publishedAt: undefined,
+          updatedAt: undefined
+        } : null
+      } : null;
+
+
+      return { ...restTransaction, duration: filteredDuration, parking: filteredParking, payment: filteredPayment };
+    });
+
+
+    return filteredRecentTransactions;
   }
 }));
